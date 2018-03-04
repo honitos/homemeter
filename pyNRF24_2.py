72f02ecf5e558115d168803019e0516da54f93f9
honitos <36992035+honitos@users.noreply.github.com>
2018-03-04 14:03:31 +0100

    Update pyNRF24_2.py

--- pyNRF24_2.py
+++ pyNRF24_2.py
@@ -10,7 +10,9 @@ import RPi.GPIO as GPIO
 
 
 def rf24_init():
+	"""Initiate the whole rf24 stuff"""
     global radio, network, mesh
+	
     #RPi B
     # Setup for GPIO 22 CE and CE1 CSN with SPI Speed @ 8Mhz
     radio = RF24(RPI_V2_GPIO_P1_22, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
@@ -24,18 +26,20 @@ def rf24_init():
     #radio.setPALevel(RF24_PA_MAX) # Power Amplifier
     radio.printDetails()
 
+def rf24_run()
     while 1:
-	mesh.update()
-	mesh.DHCP()
-	while network.available():
-	    header, payload = network.read(16)
-	    print("empfange etwas, payload ", len(payload))
-	    ms, number = unpack(">LL", bytes(payload))
+	 	mesh.update()
+		mesh.DHCP()
+		while network.available():
+	    	header, payload = network.read(16)
+		    print("empfange etwas, payload ", len(payload))
+		    ms, number = unpack(">LL", bytes(payload))
             #print("received payload ", number," at ", ms, " from", oct(header.from_node))
-	    type = header.type
-	    print(type)	
-	time.sleep(1)
+	    	type = header.type
+		    print(type)	
+		time.sleep(1)
 
 if __name__ == "__main__":
     rf24_init()
-    exit()
+	rf24_run()
+	exit()
